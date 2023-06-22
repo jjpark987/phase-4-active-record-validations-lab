72386d8775ed37104ae43f15f5adab00e078bd90
Jason Park <jjpark987@gmail.com>
2023-06-22 12:59:20 -0700

    commit tree 94199b8698866cde6a00c6a24287054a782e24f5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title
+            unless title.match?(/Won't Believe/) || title.match?(/Secret/) || title.match?(/Top\s\d+/) || title.match?(/Guess/)
+                errors.add(:title, 'Title needs more clickbait')
+            end
+        end
+    end
 end
+
